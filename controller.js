b137046d933f03eb85c52335a0ce605d0542ac5c
Blake Hagen <blakehagen@gmail.com>
2015-02-24 19:11:47 -0700

    tues 710p

--- controller.js
+++ controller.js
@@ -2,7 +2,12 @@ var app = angular.module('sounder');
 
 app.controller('MainController', function($scope, soundService) {
 
-
+	$scope.getUser = function() {
+		soundService.getUser($scope.searchText).then(function(response){
+			console.log(response.data);
+			$scope.userData = response.data;
+		})
+	}
 
 
 
