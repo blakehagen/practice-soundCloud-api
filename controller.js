8231fbc230e8e694e61967aa05756eb2a562fd53
Blake Hagen <blakehagen@gmail.com>
2015-02-24 19:45:44 -0700

    tues 745p

--- controller.js
+++ controller.js
@@ -1,12 +1,18 @@
 var app = angular.module('sounder');
 
-app.controller('MainController', function($scope, soundService) {
+app.controller('MainController', function($scope, soundService, $sce) {
 
 	$scope.getUser = function() {
 		soundService.getUser($scope.searchText).then(function(response){
-			console.log(response.data);
+			console.log(response.data)
 			$scope.userData = response.data;
-		})
+		});
+	}
+
+	$scope.play = function(track_url) {
+		SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+      		$scope.$apply($scope.player_html = $sce.trustAsHtml(oEmbed.html));
+    	});
 	}
 
 
